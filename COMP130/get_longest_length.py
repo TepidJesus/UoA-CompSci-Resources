703ffcc4b6cfe063bc35cb11fcf7d8906717666c
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2022-06-23 16:48:24 +1200

    fixed formatting.

--- COMP130/get_longest_length.py
+++ COMP130/get_longest_length.py
@@ -9,6 +9,7 @@ From a list of integers the function finds a return the length of longest consec
 def get_longest_length(values_list):
     longest_length = 0
     current_length = 0
+
     for i in range(len(values_list)):
         if values_list[i] == 0:
             current_length = 0
@@ -16,4 +17,5 @@ def get_longest_length(values_list):
             current_length += 1
             if current_length > longest_length:
                 longest_length = current_length
+                
     return longest_length
