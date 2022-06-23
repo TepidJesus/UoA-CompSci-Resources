1c3d8d9d70145189b5294f316f3de61d6e1810cd
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2022-06-23 16:47:18 +1200

    added longest_len.py
    Added another section of code the to the C130 folder.

---
+++ COMP130/get_longest_length.py
@@ -0,0 +1,19 @@
+
+"""
+
+From a list of integers the function finds a return the length of longest consecutive sequence of non-zero values.
+
+"""
+
+
+def get_longest_length(values_list):
+    longest_length = 0
+    current_length = 0
+    for i in range(len(values_list)):
+        if values_list[i] == 0:
+            current_length = 0
+        else:
+            current_length += 1
+            if current_length > longest_length:
+                longest_length = current_length
+    return longest_length
