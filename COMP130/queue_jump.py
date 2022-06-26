109a3ca9e2423714e6791e30bdd2f0579b754ec7
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2022-06-26 19:03:30 +1200

    Added queue_jump function

---
+++ COMP130/queue_jump.py
@@ -0,0 +1,27 @@
+
+"""
+
+This function takes a queue and a value. The function returns the queue but with the specified
+value at the front of the queue. If the value is not in the queue then the queue is simply returned.
+
+"""
+
+### PASTE QUEUE CLASS HERE ###
+
+def jump_queue(queue, value):
+    if queue.size() == 0:
+        return
+    oth_queue = Queue()
+    val = None
+    if value == queue.peek():
+        return 
+    while not queue.is_empty():
+        if queue.peek() == value and val == None:
+            val = queue.dequeue()
+        else:
+            oth_queue.enqueue(queue.dequeue())
+
+    if val != None:
+        queue.enqueue(val)
+    while oth_queue.size() > 0:
+        queue.enqueue(oth_queue.dequeue())
